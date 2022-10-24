d951222a6b86bada659e093652dfa484fc3d397d
= <1364096780@qq.com>
2022-10-24 16:57:30 +0800

    实现mysql的数据池联结
    锁功能暂未完善

--- CGImysql/sql_connection_pool.cpp
+++ CGImysql/sql_connection_pool.cpp
@@ -14,7 +14,127 @@
 
 using namespace std;
 
+connection_pool::connection_pool()//初始化构造函数
+{
+    m_CurConn = 0;
+    m_FreeConn = 0;
+}
+
 connection_pool *connection_pool::GetInstance() {
     static connection_pool coonPool;
     return &coonPool;
+}
+
+void connection_pool::init(string url, string User, string PassWord, string DBName, int Port, int MaxConn, int close_log) {
+    m_url = url;//主机地址
+    m_Port = Port;//端口号
+    m_User = User;//登录用户名
+    m_PassWord = PassWord;//登陆密码
+    m_DatabaseName = DBName;//使用的数据库名
+    m_close_log = close_log;//日志开关
+    for (int i = 0; i < MaxConn; i++) {
+        MYSQL *con = NULL;
+        con = mysql_init(con);//这个函数用来分配或者初始化一个MYSQL对象，用于连接mysql服务端。
+        //使用自定义分配的初始化，有可能多次调用close而导致程序崩溃是不安全的行为。
+
+        if (con == NULL) {
+            LOG_ERROR("MySQL Error");
+            exit(1);
+        }
+        con = mysql_real_connect(con, url.c_str(), User.c_str(), PassWord.c_str(), DBName.c_str(), Port, NULL, 0);
+        //使用unix连接方式，unix_socket为null时，表明不使用socket或管道机制，最后一个参数经常设置为0
+        if (con == NULL) {
+            LOG_ERROR("MySQL Error");
+            exit(1);
+        }
+        connList.push_back(con);//在连接池中添加con
+        ++m_FreeConn;//当前空闲的连接数
+    }
+
+    reserve = sem(m_FreeConn);//锁
+
+    m_MaxConn = m_FreeConn;//初始化最大数量等于当前空闲数量
+}
+
+//当有请求时，从数据库连接池中返回一个可用连接，更新使用和空闲连接数
+MYSQL *connection_pool::GetConnection()
+{
+    MYSQL *con = NULL;
+
+    if (0 == connList.size())
+        return NULL;
+
+    reserve.wait();
+
+    lock.lock();
+
+    con = connList.front();
+    connList.pop_front();
+
+    --m_FreeConn;
+    ++m_CurConn;
+
+    lock.unlock();
+    return con;
+}
+
+//释放当前使用的连接
+bool connection_pool::ReleaseConnection(MYSQL *con)
+{
+    if (NULL == con)
+        return false;
+
+    lock.lock();
+
+    connList.push_back(con);
+    ++m_FreeConn;
+    --m_CurConn;
+
+    lock.unlock();
+
+    reserve.post();
+    return true;
+}
+
+//销毁数据库连接池
+void connection_pool::DestroyPool()
+{
+
+    lock.lock();
+    if (connList.size() > 0)
+    {
+        list<MYSQL *>::iterator it;
+        for (it = connList.begin(); it != connList.end(); ++it)
+        {
+            MYSQL *con = *it;
+            mysql_close(con);//断开链接
+        }
+        m_CurConn = 0;
+        m_FreeConn = 0;
+        connList.clear();
+    }
+
+    lock.unlock();
+}
+
+//当前空闲的连接数
+int connection_pool::GetFreeConn()
+{
+    return this->m_FreeConn;
+}
+
+connection_pool::~connection_pool()
+{
+    DestroyPool();
+}
+
+connectionRAII::connectionRAII(MYSQL **SQL, connection_pool *connPool){
+    *SQL = connPool->GetConnection();
+
+    conRAII = *SQL;
+    poolRAII = connPool;
+}
+
+connectionRAII::~connectionRAII(){
+    poolRAII->ReleaseConnection(conRAII);
 }
--- CGImysql/sql_connection_pool.h
+++ CGImysql/sql_connection_pool.h
@@ -27,6 +27,35 @@ public:
     //单例模式,通过唯一的实例化来保证一个类面向系统只有一个实例，这里采用的是饿汉式实现方式
     static  connection_pool *GetInstance();
 
+    void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log);
+private:
+    connection_pool();
+    ~connection_pool();
 
+    int m_MaxConn;  //最大连接数
+    int m_CurConn;  //当前已使用的连接数
+    int m_FreeConn; //当前空闲的连接数
+    locker lock;
+    list<MYSQL *> connList; //连接池
+    sem reserve;
+
+public:
+    string m_url;			 //主机地址
+    string m_Port;		 //数据库端口号
+    string m_User;		 //登陆数据库用户名
+    string m_PassWord;	 //登陆数据库密码
+    string m_DatabaseName; //使用数据库名
+    int m_close_log;	//日志开关
+};
+
+class connectionRAII{//该类保证了
+
+public:
+    connectionRAII(MYSQL **con, connection_pool *connPool);
+    ~connectionRAII();
+
+private:
+    MYSQL *conRAII;
+    connection_pool *poolRAII;
 };
 #endif //TINYWEBSERVER_SQL_CONNECTION_POOL_H
