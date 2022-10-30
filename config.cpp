3cd3aa0e787c960694845b6a86b70a6ceaa9dc3a
= <1364096780@qq.com>
2022-10-30 23:37:14 +0800

    新增启动与配置文件

--- config.cpp
+++ config.cpp
@@ -3,3 +3,93 @@
 //
 
 #include "config.h"
+
+Config::Config(){
+    //端口号,默认9006
+    PORT = 9006;
+
+    //日志写入方式，默认同步
+    LOGWrite = 0;
+
+    //触发组合模式,默认listenfd LT + connfd LT
+    TRIGMode = 0;
+
+    //listenfd触发模式，默认LT
+    LISTENTrigmode = 0;
+
+    //connfd触发模式，默认LT
+    CONNTrigmode = 0;
+
+    //优雅关闭链接，默认不使用
+    OPT_LINGER = 0;
+
+    //数据库连接池数量,默认8
+    sql_num = 8;
+
+    //线程池内的线程数量,默认8
+    thread_num = 8;
+
+    //关闭日志,默认不关闭
+    close_log = 0;
+
+    //并发模型,默认是proactor
+    actor_model = 0;
+}
+//getopt()方法是用来分析命令行参数
+//argc：通常由 main 函数直接传入，表示参数的数量
+//argv：通常也由 main 函数直接传入，表示参数的字符串变量数组
+//*str用于参数的解析。例如 “abc:”，其中 -a，-b 就表示两个普通选项，
+//-c 表示一个必须有参数的选项，因为它后面有一个冒号。
+//全局变量optarg：如果某个选项有参数，这包含当前选项的参数字符串
+void Config::parse_arg(int argc, char*argv[]){
+    int opt;
+    const char *str = "p:l:m:o:s:t:c:a:";
+    while ((opt = getopt(argc, argv, str)) != -1)
+    {
+        switch (opt)
+        {
+            case 'p':
+            {
+                PORT = atoi(optarg);
+                break;
+            }
+            case 'l':
+            {
+                LOGWrite = atoi(optarg);
+                break;
+            }
+            case 'm':
+            {
+                TRIGMode = atoi(optarg);
+                break;
+            }
+            case 'o':
+            {
+                OPT_LINGER = atoi(optarg);
+                break;
+            }
+            case 's':
+            {
+                sql_num = atoi(optarg);
+                break;
+            }
+            case 't':
+            {
+                thread_num = atoi(optarg);
+                break;
+            }
+            case 'c':
+            {
+                close_log = atoi(optarg);
+                break;
+            }
+            case 'a':
+            {
+                actor_model = atoi(optarg);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+}
